c880d22b0918b2ef1c81a1c43a13a246f842a14e
nemasha-demel <nemashahirushidemel@gmail.com>
2025-03-14 22:05:21 +0530

    created pages for doctor list and viewing doctor profiles

---
+++ src/Pages/DoctorsListPage/DoctorsListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DoctorsListPage() {
+  return (
+    <div>DoctorsListPage</div>
+  )
+}
+
+export default DoctorsListPage
---
+++ src/Pages/ViewDoctorPage/ViewDoctorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewDoctorPage() {
+  return (
+    <div>ViewDoctorPage</div>
+  )
+}
+
+export default ViewDoctorPage
