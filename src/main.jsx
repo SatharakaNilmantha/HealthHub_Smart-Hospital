a16fa7529a739b30f6e239a93725f1daf247f0d7
Satharaka Nilmantha <158058129+SatharakaNilmantha@users.noreply.github.com>
2025-03-15 12:12:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,7 @@
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import VDoctors from './Pages/ViewDoctorsPage/ViewDoctors.jsx';
-import DoctorsList from './Pages/DoctorsListPage/DoctorsListPage.jsx';
-import AddDoctor from './Pages/AddDoctorsPage/AddDoctors.jsx';
-import './index.css';
-import App from './App.jsx';
-import Login from './Pages/LoginPage/Login';
-import Signup from './Pages/RegisterPage/Signup';
-import ViewEmployee from './Pages/ViewEmployee/ViewEmployee.jsx';
-import AddEmployeePage from './Pages/AddEmployeePage/AddEmployeePage.jsx';
-import AddDepartmentPage from './Pages/AddDepartmentPage/AddDepartmentPage.jsx';
-import DepartmentListPage from './Pages/DepartmentListPage/DepartmentListPage.jsx';
-import ViewDepartmentPage from './Pages/ViewDepartmentPage/ViewDepartmentPage.jsx';
 
-const router = createBrowserRouter([
-  { path: "/", element: <Login /> },
-  { path: "/login", element: <Login /> },
-  { path: "/signup", element: <Signup /> },
-  { path: "/dashboard", element: <App /> },
-  { path: "/doctors", element: <DoctorsList /> },
-  { path: "/adddoctor", element: <AddDoctor /> },
-  { path: "/view-doctor-profile/:id", element: <DoctorProfile /> }, // Correct route with :id
-  { path: "/employees", element: <ViewEmployee/> },
-  { path: "/addemployee", element: <AddEmployeePage/> },
-  { path: "/addDepartment", element: <AddDepartmentPage/> },
-  { path: "/DepartmentList", element: <DepartmentListPage/> },
-  { path: "/view-department", element: <ViewDepartmentPage /> },
+
+
 
 ]);
 
